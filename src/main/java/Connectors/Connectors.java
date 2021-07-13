1c0d33df6c4b57d81a2df868e40554068f07390d
JavierCanessa <javierfreddycanessa93@hotmail.es>
2021-07-13 09:59:28 -0300

    cambio de servidor

--- src/main/java/Connectors/Connectors.java
+++ src/main/java/Connectors/Connectors.java
@@ -7,13 +7,13 @@ import java.sql.DriverManager;
 public class Connectors {
     private String driver = "com.mysql.cj.jdbc.Driver";
     private String vendor = "mysql";
-    private String server = "node62718-codigomivecino.in1.cloudjiffy.net";
+    private String server = "node72799-codigomivecino1.in1.cloudjiffy.net";
     //private String server = "localhost";
     private String port = "3306";
     private String db = "listadop";
     private String params = "?serverTimezone=UTC";
     private String username = "root";
-    private String password = "XQDryl36617";
+    private String password = "SLItay86091";
     //private String password = "";
     private String url = "jdbc:" + vendor + "://" + server + ":" + port + "/" + db + params;
     private Connection conn=null;
